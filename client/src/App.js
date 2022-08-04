1d06211c8110bab4dc5abe02c799d031e7ecea85
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-08-04 07:42:42 +0530

    added basic files in client module like (index.html, App.js, index.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from './components/Clients.jsx';
+import Projects from './components/Projects.jsx';
+import Header from './components/Header.jsx';
+import AddClientModal from './components/AddClientModal.jsx';
 
-function App() {
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache,
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className=''>
+          <AddClientModal />
+          <Projects />
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
   );
-}
+};
 
 export default App;
